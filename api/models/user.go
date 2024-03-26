0fbfc6d5f7a0af8ad80940510713172b35856120
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-03-26 10:34:46 -0300

    feat: adds user model

--- api/models/user.go
+++ api/models/user.go
@@ -1,8 +1,17 @@
 package models
 
-type User struct {
-	ID       int
-	Name     string
-	Email    string
-	Password string
+import "time"
+
+type UserRequest struct {
+	Name     string `json:"name" bson:"name" validate:"required,min=3,max=60"`
+	Email    string `json:"email" bson:"email" validate:"required,email"`
+	Password string `json:"password" bson:"password" validate:"required,min=8,max=60,containsany=!@#$%&*"`
+}
+type UserCreate struct {
+	ID        string    `json:"id" bson:"id"`
+	Name      string    `json:"name" bson:"name" validate:"required,min=3,max=60"`
+	Email     string    `json:"email" bson:"email" validate:"required,email"`
+	Password  string    `json:"password" bson:"password" validate:"required,min=8,max=60,containsany=!@#$%&*"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
 }
