a351f0979b364c8b5b910f8cc36ae0b8085c27f4
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-03-26 10:33:34 -0300

    feat: initialize mongo db database

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"golang-study/api/routes"
+	"golang-study/db"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Arquivo .env não encontrado")
+	}
+
 	app := fiber.New()
 
 	// Routers config
 	routes.SetupUserRoutes(app)
+
+	db.InitDB()
 
 	// Start the HTTP server on port 5000
 	if err := app.Listen(":5000"); err != nil {
