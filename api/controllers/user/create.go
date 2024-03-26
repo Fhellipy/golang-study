ace6655a8d28ca756b97a3186af20ca7382b39b6
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-03-26 10:37:54 -0300

    feat: adds error type templates

--- api/controllers/user/create.go
+++ api/controllers/user/create.go
@@ -3,21 +3,27 @@ package user
 import (
 	"golang-study/api/models"
 	"golang-study/api/services"
+	"golang-study/api/utils"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func CreateUser(ctx *fiber.Ctx) error {
-	var user models.User
+	var user models.UserCreate
+
 	if err := ctx.BodyParser(&user); err != nil {
-		return err
+		error := models.NewBadRequestError("Alguns campos estão incorretos ou faltando")
+		return ctx.Status(error.Code).JSON(error)
 	}
 
 	us := services.NewUserService()
 
 	if err := us.CreateUser(ctx, &user); err != nil {
-		return err
+		return utils.RequestErrorValidation(ctx, err)
 	}
+
+	// remove password from response
+	user.Password = ""
 
 	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"message": "Usuário cadastrado com sucesso",
