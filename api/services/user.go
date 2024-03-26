6ae35d0bdc0d79e149087136aa0cfbaf1f7e0537
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-03-26 10:36:57 -0300

    feat: adds user validations

--- api/services/user.go
+++ api/services/user.go
@@ -1,10 +1,17 @@
 package services
 
 import (
+	"fmt"
 	"golang-study/api/models"
+	"golang-study/api/utils"
 	"golang-study/db"
+	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserService struct{}
@@ -13,11 +20,27 @@ func NewUserService() *UserService {
 	return &UserService{}
 }
 
-func (s *UserService) CreateUser(ctx *fiber.Ctx, user *models.User) error {
-	if err := s.validateUser(user); err != nil {
+func (s *UserService) CreateUser(ctx *fiber.Ctx, user *models.UserCreate) error {
+	collection := db.Database.Collection("user")
+
+	if err := s.validateUser(ctx, collection, user); err != nil {
 		return err
 	}
-	collection := db.Database.Collection("user")
+
+	user.ID = uuid.New().String()
+	timeNow := time.Now()
+	user.CreatedAt = timeNow
+	user.UpdatedAt = timeNow
+
+	// Hash password
+	passwordHashed, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return utils.RequestErrorValidation(ctx, err)
+	}
+
+	user.Password = passwordHashed
+
+	fmt.Println("collection", collection)
 
 	if _, err := collection.InsertOne(ctx.Context(), user); err != nil {
 		return err
@@ -26,14 +49,33 @@ func (s *UserService) CreateUser(ctx *fiber.Ctx, user *models.User) error {
 	return nil
 }
 
-func (s *UserService) validateUser(user *models.User) error {
-	// if user.Name == "" {
-	// 	return models.ErrNameIsRequired
-	// }
+func (s *UserService) validateUser(ctx *fiber.Ctx, collection *mongo.Collection, user *models.UserCreate) error {
+	validate := validator.New()
 
-	// if user.Email == "" {
-	// 	return models.ErrEmailIsRequired
-	// }
+	if err := validate.Struct(user); err != nil {
+		return err
+	}
+
+	filter := bson.M{"email": user.Email}
+
+	if err := collection.FindOne(ctx.Context(), filter).Decode(&user); err != nil {
+		if err == mongo.ErrNoDocuments {
+			erro := models.CustomError{
+				Key:   "bad_request",
+				Value: "email já cadastrado",
+			}
+
+			return &erro
+		}
+
+		erro := models.Error{
+			Message: "Erro ao buscar usuário",
+			Err:     "internal_server_error",
+			Code:    500,
+		}
+
+		return &erro
+	}
 
 	return nil
 }
